c75bc5463300101a7535b264d2f84c59b9f89d44
Sara Grandfield <sara.grandfield@gmail.com>
2016-09-26 15:54:12 -0400

    Initial Commit

--- ClassLibraryforEscape/Class1.cs
+++ ClassLibraryforEscape/Class1.cs
@@ -5,7 +5,59 @@ using System.Text;
 
 namespace ClassLibraryforEscape
 {
-    public class Class1
+    //Anything that counts as an object 
+    public class Entity
     {
+        
     }
+
+    //Super class for background objects
+    public class Scene
+    {
+
+    }
+
+    //Interactable thing?
+    public class Actor : Entity
+    {
+
+    }
+
+    //Object represented by artwork?
+    public class Sprite 
+    {
+
+    }
+
+    //Takes the place of treasure in the original game, sprite characters in the shape of cats
+    public class HelperFriendCat
+    {
+
+
+    }
+
+    //Objects that come into affect when a player enters the room it is in, cause an affect and have a sprite
+    public class Curse
+    {
+
+    }
+
+    //Game tiles that make up the rooms
+    public class Tile
+    {
+
+    }
+
+    //Interactable sprite supercategory of rats, player, witch, and friendcats
+    public class Character
+    {
+
+    }
+
+    // Primary antagonist, subtype of character unique atributes such as cost to kill
+    public class Rats
+    {
+
+    }
+    
 }
